6432e77618ebd92135c4a5e0f528533ec1270316
rafael mendes <rafael.mendes@tallos.com.br>
2022-10-05 16:06:12 -0300

    'resolvendo' problema do grafico

--- src/pages/Overview/Overview.vue
+++ src/pages/Overview/Overview.vue
@@ -72,8 +72,30 @@
           <VCalendar :inline="true" :showWeek="true" />
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-6" v-if="pieChart.data.series[0] !== 0">
           <chart-card :chart-data="pieChart.data" chart-type="Pie">
+            <template slot="header">
+              <h4 class="card-title">Estatisticas de dados</h4>
+              <p class="card-category">Porcentagem de documentos por tabela</p>
+            </template>
+            <template slot="footer">
+              <div class="legend">
+                <i class="fa fa-circle text-danger"></i> Comentarios
+                <i class="fa fa-circle text-info"></i> Filmes
+                <i class="fa fa-circle text-warning"></i> Teatros
+              </div>
+              <hr />
+            </template>
+          </chart-card>
+        </div>
+        <div class="col-md-6" v-else>
+          <chart-card
+            :chart-data="{
+              labels: ['F', 'C', 'T'],
+              series: [12, 20, 1],
+            }"
+            chart-type="Pie"
+          >
             <template slot="header">
               <h4 class="card-title">Estatisticas de dados</h4>
               <p class="card-category">Porcentagem de documentos por tabela</p>
@@ -93,45 +115,49 @@
   </div>
 </template>
 <script>
-  import ChartCard from 'src/components/Cards/ChartCard.vue'
-  import StatsCard from 'src/components/Cards/StatsCard.vue'
-  import LTable from 'src/components/Table.vue'
+import ChartCard from "src/components/Cards/ChartCard.vue";
+import StatsCard from "src/components/Cards/StatsCard.vue";
+import LTable from "src/components/Table.vue";
 
-  export default {
-    components: {
-      LTable,
-      StatsCard,
-      ChartCard,
-    },
-    data(){
-      return {
-          pieChart: {
-          data: {
-            labels: ['F', 'C', 'T'],
-            series: [(this.$store.state.MoviesCount / 1000),(this.$store.state.CommentsCount / 1000),(this.$store.state.TheatersCount / 1000)]
-          }
+export default {
+  components: {
+    LTable,
+    StatsCard,
+    ChartCard,
+  },
+  data() {
+    return {
+      pieChart: {
+        data: {
+          labels: ["F", "C", "T"],
+          series: [
+            this.$store.state.MoviesCount / 1000,
+            this.$store.state.CommentsCount / 1000,
+            this.$store.state.TheatersCount / 1000,
+          ],
         },
-        Movies: "",
-        Theaters: "",
-        Users: "",
-        Comments: "",
-        Sessions: ""
-      }
-    },
-    async created(){
-      const jwt = localStorage.getItem("token");
-      await this.$store.dispatch("getMoviesCount", `Bearer ${jwt}`);
-      await this.$store.dispatch("getTheatersCount", `Bearer ${jwt}`);
-      await this.$store.dispatch("getUsersCount", `Bearer ${jwt}`);
-      await this.$store.dispatch("getCommentsCount", `Bearer ${jwt}`);
-      await this.$store.dispatch("getSessionsCount", `Bearer ${jwt}`);
-        this.Movies = await this.$store.state.MoviesCount,
-        this.Theaters = await this.$store.state.TheatersCount,
-        this.Users = await this.$store.state.UsersCount,
-        this.Comments = await this.$store.state.CommentsCount,
-        this.Sessions = await this.$store.state.SessionsCount
-    },
-  }
+      },
+      Movies: "",
+      Theaters: "",
+      Users: "",
+      Comments: "",
+      Sessions: "",
+    };
+  },
+  async created() {
+    const jwt = localStorage.getItem("token");
+    await this.$store.dispatch("getMoviesCount", `Bearer ${jwt}`);
+    await this.$store.dispatch("getTheatersCount", `Bearer ${jwt}`);
+    await this.$store.dispatch("getUsersCount", `Bearer ${jwt}`);
+    await this.$store.dispatch("getCommentsCount", `Bearer ${jwt}`);
+    await this.$store.dispatch("getSessionsCount", `Bearer ${jwt}`);
+    (this.Movies = await this.$store.state.MoviesCount),
+      (this.Theaters = await this.$store.state.TheatersCount),
+      (this.Users = await this.$store.state.UsersCount),
+      (this.Comments = await this.$store.state.CommentsCount),
+      (this.Sessions = await this.$store.state.SessionsCount);
+  },
+};
 </script>
 <style>
 </style>
